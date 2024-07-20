998f1fec24a565797f231be7618128246b1ad8ef
amittrivedi565 <edgeparallel@gmail.com>
2024-07-20 09:43:29 +0530

    routing changes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,9 +3,9 @@ const adminRoutes = require("./admin.routes");
 const openRoutes = require("./open.routes");
 
 /* Admin Routes (require auth) */
-router.use("/api/close", adminRoutes);
+router.use("/close", adminRoutes);
 
 /* Common or Open Routes (anyone can access) */
-router.use("/api/open", openRoutes);
+router.use("/open", openRoutes);
 
 module.exports = router;
