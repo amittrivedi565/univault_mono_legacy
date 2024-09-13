366082409922dce5d7a30efcc1e659ba1a1e1aa2
Deevanshu Kushwah <astroboiscosmos@gmail.com>
2024-09-13 16:05:41 +0530

    data layer is connected to admin dashboard

--- controllers/admin/admin.controller.js
+++ controllers/admin/admin.controller.js
@@ -1,8 +1,23 @@
 const db = require("../../models");
 exports.adminGet = {
     controller: async (req, res) => {
-        const adminData = await db.admins.findAll({})
-        const Param = req.query 
-        res.render("../views/admin/dashboard.ejs",{adminData , title : "Dashboard"})
-  }};
-  
+        const universities = await db.university.count();
+        const courses = await db.courses.count();
+        const branches = await db.branches.count();
+        const subjects = await db.subjects.count();
+        const notes = await db.unit.count();
+        const admins = await db.admins.count();
+
+        res.render("../views/admin/dashboard.ejs", {
+            count: {
+                universities,
+                courses,
+                branches,
+                subjects,
+                notes,
+                admins,
+            },
+            title: "Dashboard",
+        });
+    },
+};
